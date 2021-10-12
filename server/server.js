88a2f74137e91af97206630b94485f493a1a9a9e
s.claes <sebastian.claes@protonmail.com>
2021-10-12 17:20:53 +0200

    [ADD] outsourced addPlayer and updatePlayers function and made some more comments

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const socketio = require('socket.io')(http, {
     pingTimeout: 60000
 });
 
+
 let players = {
     player1: {
         "name": "Sebastian",
@@ -18,21 +19,46 @@ let players = {
     }
 }
 
-// new socket connection
-socketio.on('connection', (socket) => {
 
+/**
+ * Start a new socket connection
+ */
+socketio.on('connection', (socket) => {
+    updatePlayers(socket)
+    addPlayer(socket);
+});
+
+
+/**
+ * Emit the players object to the client
+ * @param socket
+ */
+function updatePlayers(socket) {
     // emit all players to the client
     socket.emit("players", players);
+}
 
-    // receive players
+
+/**
+ * Add a player to the players object
+ * @param socket
+ */
+function addPlayer(socket) {
+    // on addPlayer method call
     socket.on("addPlayer", data => {
         // get the players object length and increase it with one
         let count = Object.keys(players).length + 1;
         // add player to the players object
-        players['player'+ count] = data
+        players['player'+ count] = data;
+        // emit all players to the client
+        socket.emit("players", players);
     })
-});
+}
 
+
+/**
+ * Start the server on port 3030
+ */
 http.listen(3030, () => {
     console.log('Server started on port 3030...');
 });
