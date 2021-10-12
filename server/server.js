e6ba0cec874f6e36239cb39e3cec53e4805b8b4e
s.claes <sebastian.claes@protonmail.com>
2021-10-12 14:55:53 +0200

    [ADD] declared express, http and socketio constants in the server.js

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const http = require('http').Server(express);
+const socketio = require('socket.io')(http, {
+    cors: {
+        origin: "*"
+    },
+    pingTimeout: 60000
+});
