1f51f148c8d66015dae63f0a6db3d020403774e7
s.claes <sebastian.claes@protonmail.com>
2021-10-13 14:56:10 +0200

    [NEW] created the player partial component and added basic html structure

---
+++ client/src/components/partials/Player.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="player-wrapper">
+    <div class="player-color"></div>
+    <div class="player-image"></div>
+    <div class="player-name">
+      <p>Sebastian</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import io from "socket.io-client"
+export default {
+  name: 'Player',
+  data () {
+    return {
+      socket: {},
+    }
+  },
+  created () {
+    this.socket = io(this.$hostname);
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
