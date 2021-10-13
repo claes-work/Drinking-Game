c01d4467860028b4ed9931d3425f2fdd8748a29e
s.claes <sebastian.claes@protonmail.com>
2021-10-13 11:20:43 +0200

    [ADD] global hostname so that I have access to the hostname variable in every component and that I can easily change between development and production mode

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
+import {createApp, VueElement} from 'vue'
 import App from './App.vue'
 
+// the server hostname
+VueElement.prototype.$hostname = 'http://192.168.0.104:3030/'
+
 createApp(App).mount('#app')
+
+
+
