516379cb57755ed4e17d5aeda8efc0bb0207dec2
s.claes <sebastian.claes@protonmail.com>
2021-10-13 14:55:25 +0200

    [NEW] created the join form template component and added showMainMenu() method

---
+++ client/src/components/partials/JoinForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="form-wrapper join">
+    <button @click="showMainMenu">Hauptmenü</button>
+  </div>
+</template>
+
+<script>
+import io from "socket.io-client"
+export default {
+  name: 'JoinForm',
+  data () {
+    return {
+      socket: {},
+    }
+  },
+  created () {
+    this.socket = io(this.$hostname);
+  },
+  methods: {
+    showMainMenu() {
+      this.$parent.showButtonWrapper();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
