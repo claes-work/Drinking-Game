0f558dbce5a15176a2bc631b43b661810008d8e0
s.claes <sebastian.claes@protonmail.com>
2021-10-13 10:09:20 +0200

    [ADD] created dynamic component in App.vue to switch between the main view components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <Game />
+  <component @nextComponent="changeComponent" :is="selectedComponent"></component>
 </template>
 
 <script>
-import './css/general.css'
+import './css/general.css';
+import Join from './components/Join.vue'
+import Room from './components/Room.vue'
 import Game from './components/Game.vue'
+import io from "socket.io-client";
 
 export default {
   name: 'App',
   components: {
-    Game
+    Join,
+    Room,
+    Game,
+  },
+  data () {
+    return {
+      selectedComponent: "Room"
+    }
+  },
+  created () {
+    this.socket = io("http://192.168.0.104:3030/");
+  },
+  methods: {
+    changeComponent (component){
+      this.selectedComponent = component;
+    }
   }
 }
 </script>
