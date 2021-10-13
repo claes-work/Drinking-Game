87351da7b328d8cfe92ba6e2a347e3539b8b64be
s.claes <sebastian.claes@protonmail.com>
2021-10-13 09:13:44 +0200

    [NEW] created join vue component

---
+++ client/src/components/Join.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import io from "socket.io-client"
+export default {
+  name: 'Join',
+  data () {
+    return {
+      socket: {},
+    }
+  },
+  created () {
+    this.socket = io("http://192.168.0.104:3030/");
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
