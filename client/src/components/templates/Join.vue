6a01a4d12727cd4f70e159e551765cb0b1784ad3
s.claes <sebastian.claes@protonmail.com>
2021-10-13 15:11:23 +0200

    [ADD] button-wrapper, joinForm and createForm partial component to the Join template partial

--- client/src/components/templates/Join.vue
+++ client/src/components/templates/Join.vue
@@ -1,16 +1,35 @@
 <template>
   <div class="section join">
-    <div class="container"></div>
+    <div class="container">
+
+      <div class="button-wrapper" v-if="buttonWrapper">
+        <button @click="showCreateForm">Spiel erstellen</button>
+        <button @click="showJoinForm">Spiel beitreten</button>
+      </div>
+
+      <CreateForm v-if="createForm"></CreateForm>
+      <JoinForm v-if="joinForm"></JoinForm>
+
+    </div>
   </div>
 </template>
 
 <script>
+import CreateForm from '../partials/CreateForm.vue'
+import JoinForm from '../partials/JoinForm.vue'
 import io from "socket.io-client"
 export default {
   name: 'Join',
+  components: {
+    CreateForm,
+    JoinForm,
+  },
   data () {
     return {
       socket: {},
+      buttonWrapper: true,
+      createForm: false,
+      joinForm: false,
     }
   },
   created () {
