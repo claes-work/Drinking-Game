dc3acea19d75e48d7ec5d166917eda9b2ee44bf9
s.claes <sebastian.claes@protonmail.com>
2021-10-13 14:55:11 +0200

    [NEW] created the create form template component and added showMainMenu() method

---
+++ client/src/components/partials/CreateForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="form-wrapper create">
+      <button @click="showMainMenu">Hauptmenü</button>
+  </div>
+</template>
+
+<script>
+import io from "socket.io-client"
+export default {
+  name: 'CreateForm',
+  data () {
+    return {
+      socket: {},
+    }
+  },
+  created () {
+    this.socket = io(this.$hostname);
+  },
+  methods: {
+    showMainMenu() {
+      this.$parent.showButtonWrapper();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
