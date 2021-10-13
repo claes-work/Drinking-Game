053d1272f59804e8ef0e357876039bed677a586a
s.claes <sebastian.claes@protonmail.com>
2021-10-13 14:57:42 +0200

    [NEW] created the dice partial component

---
+++ client/src/components/partials/Dice.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="player-wrapper">
+
+  </div>
+</template>
+
+<script>
+import io from "socket.io-client"
+export default {
+  name: 'Dice',
+  data () {
+    return {
+      socket: {},
+    }
+  },
+  created () {
+    this.socket = io(this.$hostname);
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
