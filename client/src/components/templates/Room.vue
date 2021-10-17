f2654eec551d980f8cd139f472339dba1a5f2cf0
s.claes <sebastian.claes@protonmail.com>
2021-10-17 14:59:51 +0200

    [ADD] Room.vue template structure and a mounted function that assign the rooms object variables

--- client/src/components/templates/Room.vue
+++ client/src/components/templates/Room.vue
@@ -1,27 +1,44 @@
 <template>
-  <div class="section room">
-    <div class="container">
-      <Player></Player>
+  <div class="section wooden">
+    <div class="container flex">
+      <div class="poster bc-atts">
+        <div class="knop left bc-atts"></div>
+        <div class="knop right bc-atts"></div>
+        <h3>RAUM CODE</h3>
+        <h2>{{roomCode}}</h2>
+        <div class="divider"></div>
+        <h3>SPIELER INFORMATIONEN</h3>
+        <div class="player-container">
+          <Player v-for="(player, index) in players" :key="index" :playerName="player.username" :color="player.color"/>
+        </div>
+        <div class="button-wrapper">
+          <div class="button green">SPIEL STARTEN</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import io from "socket.io-client"
 import Player from "../partials/Player";
 
 export default {
   name: 'Room',
   components: {
-    Player,
+    Player
   },
   data () {
     return {
-      socket: {},
+      socket: this.$parent.socket,
+      roomObject: {},
+      roomCode: '',
+      players: []
     }
   },
-  created () {
-    this.socket = io(this.$hostname);
+  mounted() {
+    this.roomObject = this.$parent.roomData
+    this.roomCode = this.$parent.roomData.roomCode
+    this.players = this.$parent.roomData.playersArray
   },
   methods: {
     changeComponent (){
