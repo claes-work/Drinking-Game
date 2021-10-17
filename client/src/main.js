e7aa20ff105328ad476b5d3b6032409744db5969
s.claes <sebastian.claes@protonmail.com>
2021-10-17 14:50:47 +0200

    [REMOVE] removed the global hostname variable from the vue main.js file, because it wasn't working and I'm inheriting the variable now

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,5 @@
-import {createApp, VueElement} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 
-// the server hostname
-VueElement.prototype.$hostname = 'http://192.168.0.104:3030/'
-
 createApp(App).mount('#app')
-
-
 
