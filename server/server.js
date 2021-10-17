234c4cab6a2024a1180f66fe9b6084a07eff246e
s.claes <sebastian.claes@protonmail.com>
2021-10-17 14:30:38 +0200

    [REFACTOR] the create room function will now expand the new servers room list array

--- server/server.js
+++ server/server.js
@@ -1,61 +1,37 @@
+const Room   = require('./classes/model/Room.js');
+const Player = require('./classes/model/Player.js');
+
 const express = require('express');
 const http = require('http').Server(express);
 const io = require('socket.io')(http, {
     cors: {
-        origin: "*"
+        origin: '*'
     },
     pingTimeout: 60000
 });
 
-/*********************************************************************/
-/* -------------------------- Variables ---------------------------- */
-/*********************************************************************/
-
-let players = {
-    player1: {
-        "name": "Sebastian",
-        "color": "green",
-    },
-    player2: {
-        "name": "Adrian",
-        "color": "blue",
-    }
-}
+/**
+ * Array of room objects
+ * @type {*[]}
+ */
+let roomList = [];
 
 /**
  * Start a new socket connection
  */
 io.on('connection', (socket) => {
-    updatePlayers()
-    addPlayer(socket);
+
+    // Check if Player is in party and if so redirect him
+
+    // Check if submitted player data is valid and create a new room
+    socket.on('createRoom', (username) => {
+        let room = createRoom(username);
+        roomList.push(room);
+        socket.emit('room', room);
+    });
+
+
 });
-
-
-/**
- * Emit the players object to the client
- */
-function updatePlayers() {
-    // emit all players to the client
-    io.emit("players", players);
-}
-
-
-/**
- * Add a player to the players object
- * @param socket
- */
-function addPlayer(socket) {
-    // on addPlayer method call
-    socket.on("addPlayer", data => {
-        // get the players object length and increase it with one
-        let count = Object.keys(players).length + 1;
-        // add player to the players object
-        players['player'+ count] = data;
-        // emit all players to the client
-        updatePlayers();
-    })
-}
-
 
 /**
  * Start the server on port 3030
@@ -63,3 +39,27 @@ function addPlayer(socket) {
 http.listen(3030, () => {
     console.log('Server started on port 3030...');
 });
+
+
+/**
+ * Check if submitted player data is valid and create a new room
+ * @param username
+ */
+function createRoom(username) {
+
+    let room = new Room(
+        1,
+        0,
+        [1]
+    );
+
+    let player = new Player(
+        1,
+        username,
+        room.assignColor(),
+        0
+    );
+
+    room.addPlayer(player)
+    return room;
+}
