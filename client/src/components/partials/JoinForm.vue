bce270d607e4b8ae7df403b1f5111289fcc78a0e
s.claes <sebastian.claes@protonmail.com>
2021-10-17 14:42:16 +0200

    [ADD] join form template elements and some empty room variables

--- client/src/components/partials/JoinForm.vue
+++ client/src/components/partials/JoinForm.vue
@@ -1,25 +1,35 @@
 <template>
-  <div class="form-wrapper join">
+  <div class="form-wrapper">
     <button @click="showMainMenu">Hauptmenü</button>
+    <div class="input-wrapper">
+      <div class="input-icon home bc-atts"></div>
+      <input type="text" v-model="roomCode" id="room-code" class="room-code-input" placeholder="RAUM CODE" />
+    </div>
+    <div class="input-wrapper">
+      <div class="input-icon user bc-atts"></div>
+      <input type="text" v-model="username" id="username" class="username-input" placeholder="DEIN NAME" />
+    </div>
+    <span class="error-message" v-if="errorMessage !== ''">{{errorMessage}}</span>
+    <button @click="joinRoom" class="button join">Raum Beitreten</button>
   </div>
 </template>
 
 <script>
-import io from "socket.io-client"
 export default {
   name: 'JoinForm',
   data () {
     return {
-      socket: {},
+      socket: this.$parent.socket,
+      roomCode: '',
+      username: '',
+      errorMessage: '',
     }
   },
-  created () {
-    this.socket = io(this.$hostname);
-  },
+
   methods: {
-    showMainMenu() {
-      this.$parent.showButtonWrapper();
-    }
+    showMainMenu () {
+      this.$parent.showButtonWrapper ();
+    },
   }
 }
 </script>
