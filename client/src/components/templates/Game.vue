a567f0c4dcdfc8953392b3cb0d9b764fbe9d3be9
s.claes <sebastian.claes@protonmail.com>
2021-10-17 14:51:46 +0200

    [REMOVE] removed the addPlayer function, because it was just for testing purposes

--- client/src/components/templates/Game.vue
+++ client/src/components/templates/Game.vue
@@ -1,45 +1,25 @@
 <template>
   <div class="section game">
     <div class="container">
-      <p>{{players}}</p>
-      <button @click="addPlayer">Push Player</button>
+
     </div>
   </div>
 </template>
 
 <script>
-import io from "socket.io-client"
+
 export default {
   name: 'Game',
   data () {
     return {
-      socket: {},
-      players: {},
+      socket: this.$parent.socket,
     }
-  },
-  created () {
-    this.socket = io(this.$hostname);
-  },
-  mounted () {
-    // receive players from the server.js
-    this.socket.on("players", data => {
-      this.players = data
-    });
   },
   methods: {
-    // add player to the players object
-    addPlayer() {
-        let playerObject = {
-          "name": "Tom",
-          "color": "orange",
-        }
-        // emit the playerObject to the server.js
-        this.socket.emit("addPlayer", playerObject)
-    }
+    changeComponent () {
+      this.$emit("nextComponent", "Game");
+    },
   },
-  changeComponent (){
-    this.$emit("nextComponent", "Game");
-  }
 }
 </script>
 
