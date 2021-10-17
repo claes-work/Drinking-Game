8e4d486713e7ebaf7f624c23bc0ec75e5bb2f6b8
s.claes <sebastian.claes@protonmail.com>
2021-10-17 15:01:40 +0200

    [NEW] created a player class model that will be used by the server

---
+++ server/classes/model/Player.js
@@ -0,0 +1,15 @@
+class Player {
+
+    constructor (
+        uid,
+        username,
+        color,
+        diceCount,
+    ) {
+        this.uid       = uid;
+        this.username  = username;
+        this.color     = color;
+        this.diceCount = diceCount
+    }
+}
+module.exports = Player
